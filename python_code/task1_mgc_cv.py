a3ea0c6b672946d15aaa9552a4fbdb6ba2637fa4
Apurv <apurvmishra99@gmail.com>
2020-04-08 16:57:34 +0530

    k_fold partioning done

--- python_code/task1_mgc_cv.py
+++ python_code/task1_mgc_cv.py
@@ -4,6 +4,39 @@
 import numpy as np
 import scipy.io
 
+def _split(X, k, mc):
+    curr = 1
+    sub_arrays = []
+    for i in range(k):
+        if curr != k:
+            start = i*mc
+            end = i*mc + mc
+            sub_arrays.append(X[start:end])
+        else:
+            start = i*mc
+            sub_arrays.append(X[start:])
+        curr += 1
+    return sub_arrays
+
+def my_k_folds(X, Y, k_folds):
+    # Assuming classes follow the pattern 1..C;
+    # C becomes the total no. of classes
+    no_of_classes = np.max(Y)
+    p_map = np.zeros((len(X), 1))
+    
+    for c in range(1, no_of_classes+1):
+        Nc = np.count_nonzero(Y == c)
+        Mc = int(np.floor(Nc / k_folds))
+        # find indexes of all places where the element is c
+        idxs = np.apply_along_axis(lambda x: x[0], 1, np.argwhere(Y == c))
+        # split the idxs based on k_folds and Mc
+        sub_arrs = _split(idxs, k_folds, Mc)
+        for i in range(len(sub_arrs)):
+            p_map[sub_arrs[i]] = i + 1
+            
+    return p_map
+
+
 def task1_mgc_cv(X, Y, CovKind, epsilon, Kfolds):
     # Input:
     #  X : N-by-D matrix of feature vectors (np.double)
