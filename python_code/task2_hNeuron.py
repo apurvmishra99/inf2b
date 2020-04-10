69e2ec8e62059f2d9e0cbd0d41ca52693dea1320
Apurv <apurvmishra99@gmail.com>
2020-04-10 17:45:33 +0530

    2.1 and 2.2

--- python_code/task2_hNeuron.py
+++ python_code/task2_hNeuron.py
@@ -4,11 +4,21 @@
 import numpy as np
 import scipy.io
 
+
+def step(y):
+    return 1 * (y > 0)
+
+
 def task2_hNeuron(W, X):
     # Input:
     #  X : N-by-D matrix of input vectors (in row-wise) (np.double)
     #  W : (D+1)-by-1 vector of weights (np.double)
     # Output:
     #  Y : N-by-1 vector of output (np.double)
-
-    return Y
+    pad = np.ones(X.shape[0])
+    # X_padded shape == (D+1 x N)
+    X_padded = np.vstack((pad, X.T))
+    # W.T shape == (1 x D+1)
+    Y = (W.T @ X_padded).T
+    # Y shape == (D+1 x 1)
+    return step(Y)
--- python_code/task2_sNeuron.py
+++ python_code/task2_sNeuron.py
@@ -4,11 +4,22 @@
 import numpy as np
 import scipy.io
 
+
+def sigmoid(x):
+    # Our activation function: f(x) = 1 / (1 + e^(-x))
+    return 1 / (1 + np.exp(-x))
+
+
 def task2_sNeuron(W, X):
     # Input:
     #  X : N-by-D matrix of input vectors (in row-wise) (np.double)
     #  W : (D+1)-by-1 vector of weights (np.double)
     # Output:
     #  Y : N-by-1 vector of output (np.double)
-
-    return Y
+    pad = np.ones(X.shape[0])
+    # X_padded shape == (D+1 x N)
+    X_padded = np.vstack((pad, X.T))
+    # W.T shape == (1 x D+1)
+    Y = (W.T @ X_padded).T
+    # Y shape == (D+1 x 1)
+    return sigmoid(Y)
