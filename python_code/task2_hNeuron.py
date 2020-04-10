1971f52ba0a31190ca5efaf62370ea21e7506b5e
Apurv <apurvmishra99@gmail.com>
2020-04-10 18:06:08 +0530

    better way 2.1 and 2.2

--- python_code/task2_hNeuron.py
+++ python_code/task2_hNeuron.py
@@ -15,10 +15,11 @@ def task2_hNeuron(W, X):
     #  W : (D+1)-by-1 vector of weights (np.double)
     # Output:
     #  Y : N-by-1 vector of output (np.double)
-    pad = np.ones(X.shape[0])
-    # X_padded shape == (D+1 x N)
-    X_padded = np.vstack((pad, X.T))
-    # W.T shape == (1 x D+1)
-    Y = (W.T @ X_padded).T
+
+    bias = W[0]
+    W = np.delete(W, 0, 0)
+    # W.T shape == (1 x D)
+    # X.T shape == (D x N)
+    Y = (W.T @ X.T).T + bias
     # Y shape == (D+1 x 1)
     return step(Y)
